bcb4c5a2caae1475566b429adff6fa1d9ed201b2
Aleksejs Jurenoks <40733171+ajurenoks@users.noreply.github.com>
2023-07-16 20:57:39 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,13 +1,8 @@
 from openpyxl import Workbook, load_workbook 
 wb=load_workbook('tests/test1.xlsx')
 ws=wb.active
-max_row=ws.max_row
 total=0
-for row in range(2,max_row+1):
-    hour=ws['B' + str(row)].value
-    rate=ws['C' + str(row)].value
-    salary=hour*rate
-    if (salary>3000):
-        daudzums=daudzums+1
+#write your code here
+
 print(total)
 wb.close()
